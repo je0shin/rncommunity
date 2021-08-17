df987a3f5941ec06cd6bfa45006104a59b9955bc
Jo <neutrino400@gmail.com>
2021-08-17 21:14:36 +0900

    [Update] Added posting fjn.

--- components/freeTalk/FreeMain.js
+++ components/freeTalk/FreeMain.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import { View, Text } from 'react-native';
+import firestore from '@react-native-firebase/firestore'
+import auth from '@react-native-firebase/auth'
 
 export default function FreeMain() {
+    const [feed, setFeed] = useState(null)
+    
+    useEffect(() => {
+        
+    }, [])
     return(
         <View>
             
--- components/freeTalk/PostChat.js
+++ components/freeTalk/PostChat.js
@@ -1,32 +1,65 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, TextInput, StyleSheet, Image, Button } from "react-native";
+// import { useDispatch, useSelector } from 'react-redux';
+import firestore from '@react-native-firebase/firestore'
+import auth from '@react-native-firebase/auth'
 import { launchImageLibrary } from "react-native-image-picker";
 
-const postChat = () => {
+// function getImageFromLib(setImage) {
+//     const options = {
+//         quality: 1,
+//     }
+
+//     launchImageLibrary(options, (responseObj) => {
+//         if (responseObj.didCancel) {
+//             console.log('User cancelled')
+//         } else if (responseObj.errorCode) {
+//             console.log('error code' + responseObj.errorCode)
+//         } else if (responseObj.assets) {
+//             setImage(responseObj.uri)
+//         }
+//         console.log(responseObj.uri)
+
+//     })
+// }
+
+const postChat = (text) => {
+    firestore().collection("freeChat")
+        .add({
+            content: text,
+            dislikeCount: 0,
+            likeCount: 0,
+            posted: firestore.FieldValue.serverTimestamp(),
+            user: firestore().collection("users").doc(auth().currentUser.uid),
+        })
+        .then(() => {
+            console.log('User added!');
+          });
     console.log("psot pressed")
 }
 
-function AddImageSection() {
-    const [image, setImage] = useState(null)
-    const [buttonText, setButtonText] = useState("Add Image")
+// function AddImageSection() {
+//     const [image, setImage] = useState(null)
+//     const [buttonText, setButtonText] = useState("Add Image")
 
-    useEffect(() => {
-        if (image) {
-            setButtonText("Change Image")
-        } else {
-            setButtonText("Add Image")
-        }
-    }, [image])
+//     useEffect(() => {
+//         console.log(image)
+//         if (image) {
+//             setButtonText("Change Image")
+//         } else {
+//             setButtonText("Add Image")
+//         }
+//     }, [image])
 
-    return(
-        <View>
-            <Button
-              onPress={() => console.log("add pressed")}
-              title={buttonText}  
-            />
-        </View>
-    )
-}
+//     return(
+//         <View>
+//             <Button
+//               onPress={() => getImageFromLib(setImage)}
+//               title={buttonText}  
+//             />
+//         </View>
+//     )
+// }
 
 export default function PostChat() {
     const [text, setText] = useState(null)
@@ -37,9 +70,9 @@ export default function PostChat() {
                 placeholder="Enter your chat here"
                 onChangeText={(val) => setText(val)}
             />
-            <AddImageSection/>
+            {/* <AddImageSection/> */}
             <Button
-                onPress={() => postChat()}
+                onPress={() => postChat(text)}
                 title="Post"
             />
         </View>
