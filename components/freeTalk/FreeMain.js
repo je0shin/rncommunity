2e3e2d5eabcc834c692ccfd3f8abf3d1fb6cd313
Jo <neutrino400@gmail.com>
2021-08-23 23:26:12 +0900

    no

--- components/freeTalk/FreeMain.js
+++ components/freeTalk/FreeMain.js
@@ -1,10 +1,35 @@
 import React from 'react'
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, TextInput } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+import { useState } from 'react'
 
 export default function FreeMain() {
+    const [theText, setTheText] = useState({scissor: "",
+                                            rock: "",
+                                            paper: ""})
     return(
         <View>
-            <Text> This is main </Text>
+            <FlatList/>
+            <View>
+                <Text>{theText.scissor}</Text>
+                <TextInput 
+                    placeholder="Enter message here..."
+                    value={theText.scissor}
+                    onChangeText={(val) => setTheText({...theText, scissor: val})}
+                />
+                <Text>{theText.rock}</Text>
+                <TextInput 
+                    placeholder="Enter message here..."
+                    value={theText.rock}
+                    onChangeText={(val) => setTheText({...theText, rock: val})}
+                />
+                <Text>{theText.paper}</Text>
+                <TextInput 
+                    placeholder="Enter message here..."
+                    value={theText.paper}
+                    onChangeText={(val) => setTheText({...theText, paper: val})}
+                />
+            </View>
         </View>
     )
 }
